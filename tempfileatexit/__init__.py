64dbf13ee63329c3d9be20b6431c2ff8fc7777ab
Jachym Cepicky <jachym.cepicky@gmail.com>
2020-07-24 09:34:26 +0200

    adding mkdtemp and mkstemp

--- tempfileatexit/__init__.py
+++ tempfileatexit/__init__.py
@@ -3,10 +3,13 @@ import collections
 import time
 import os
 import shutil
+import tempfile
 
 __FILES__ = []
 
 def remove():
+    """Remove all registered files and directories
+    """
     global __FILES__
 
     for f in __FILES__:
@@ -23,6 +26,8 @@ atexit.register(remove)
 
 
 def register(fobj):
+    """Register new file or directory
+    """
     global __FILES__
     if os.path.isfile(fobj) or\
       os.path.isdir(fobj):
@@ -32,6 +37,8 @@ def register(fobj):
 
 
 def list(verbose=False):
+    """List registered objects
+    """
     if not verbose:
         return __FILES__
     else:
@@ -50,6 +57,20 @@ def list(verbose=False):
                 data[f]["type"] = "dir"
 
         return data
+
+def mkdtemp(*args, **kwargs):
+    """Call tempfile.mkdtemp and register it to be removed at exit
+    """
+    new_dir = tempfile.mkdtemp(*args, **kwargs)
+    register(new_dir)
+    return new_dir
+
+def mkstemp(*args, **kwargs):
+    """Call tempfile.mkstemp and register it to be removed at exit
+    """
+    (fd, name) = tempfile.mkstemp(*args, **kwargs)
+    register(name)
+    return (fd, name)
 
 class TempFileAtExitException(Exception):
     pass
